c22dbe4cf2b3d64c569c3bf30ea03ac8e33dbe2a
Napoleon Maraidonis <napo95@gmail.com>
2017-01-05 17:08:31 +0200

    Added Better Example
    Whoops

--- example.py
+++ example.py
@@ -1,10 +1,21 @@
+def x(x=0,aa=1):
+	return kappa
 
+x = 2
 
-x++
-def x():
-	y = 5
-def x():
-	k =10
+print x + x
 
+x -= 1
+x /= 2
 
+u = 1.0
+d = 'String'
+myList = ['apple' , 'windows' ,'BSD']
+if 1==1:
+	print 3+2*3*4
+for i in kappa:
+	print i
+kappa = 1
+while kappa==1:
+	kappa = 0
 
